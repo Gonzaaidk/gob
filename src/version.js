68f2d11f9698b3e332721ceaf19066e9a714268d
Gonzaaidk <85621947+Gonzaaidk@users.noreply.github.com>
2021-07-26 21:55:04 -0300

    Add files via upload

---
+++ src/version.js
@@ -0,0 +1,15 @@
+const version = (prefix, pushname) => {
+    return `
+*Gσηzααвσт ву Gσηzαα ι∂к*
+
+*Actualizado:* 26 de julio de 2021
+*Versión actual:* 3.2
+*Ofrecida por:* Gσηzαα ι∂к™
+
+
+ву Gσηzαα ι∂к
+`
+
+}
+
+exports.version = version
